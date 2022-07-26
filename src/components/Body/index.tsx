bdc1187a42fd078753ae4fe79ea41159b350b1b9
dfilastro <d.filastro@hotmail.com>
2022-07-26 15:05:12 -0300

    axios/api integration

--- src/components/Body/index.tsx
+++ src/components/Body/index.tsx
@@ -1,38 +1,72 @@
 import './styles.css';
 import hambImg from '../../assets/foto.png';
 import ExtraItens from '../ExtraItens';
-import PlusLessButton from '../PlusLessButton';
-import AddButton from '../AddButton';
+import PlusMinusButton from '../PlusMinusButton';
+import Button from '../Button';
+import { useCart } from '../../hooks/useCart';
+import api from '../../services/api';
+import { useEffect, useState } from 'react';
+import { AxiosResponse } from 'axios';
+import LoadingSpinner from '../IsLoading';
 
 export function Body() {
+  const { addProduct } = useCart();
+  let qty = 0;
+
+  const [data, setData] = useState<any>({});
+  const [isLoading, setIsLoading] = useState(true);
+
+  useEffect(() => {
+    api.get('').then(({ data }: AxiosResponse<any>) => {
+      setData(data[0]);
+      setIsLoading(false);
+    });
+  }, []);
+
+  const hamburguer = {
+    id: data.id,
+    product: data.nm_product,
+    description: data.description,
+    price: new Intl.NumberFormat('pt-BR', { style: 'currency', currency: 'BRL' }).format(
+      data.vl_price
+    ),
+    promo: new Intl.NumberFormat('pt-BR', { style: 'currency', currency: 'BRL' }).format(
+      data.vl_discount
+    ),
+    img: data.url_image,
+    extras: data.ingredients ? data.ingredients[0].itens : [],
+    maxItens: data.ingredients ? data.ingredients[0].max_itens : [],
+    cutlery: data.ingredients ? data.ingredients[1].group : [],
+  };
+
+  if (isLoading) return <LoadingSpinner />;
+
   return (
     <main className='c-body'>
       <div className='c-body__product'>
         <div className='c-body__product__c-img'>
           <img className='c-body__product__c-img__img' src={hambImg} alt='' />
         </div>
-        <h1 className='c-body__product__tittle'>Oferta Picanha Cheddar Bacon</h1>
-        <p className='c-body__product__description'>
-          Hambúrguer de picanha, molho de picanha, cebola crispy, bacon, queijo cheddar, molho
-          cheddar e pão mix de gergelim
-        </p>
+        <h1 className='c-body__product__tittle'>{hamburguer.product}</h1>
+        <p className='c-body__product__description'>{hamburguer.description}</p>
         <div className='c-body__product__c-price'>
-          <p>R$ 32,99</p>
-          <p className='c-body__product__c-price__old'>R$ 34,95</p>
+          <p>{hamburguer.promo}</p>
+          <p className='c-body__product__c-price__old'>{hamburguer.price}</p>
         </div>
       </div>
 
       <div className='c-body__ingredients'>
         <div className='c-body__ingredients__c-extras'>
           <p>Adicionar Ingredientes</p>
-          <p className='c-body__ingredients__c-extras_q'>Até 8 ingredientes</p>
+          <p className='c-body__ingredients__c-extras_q'>Até {hamburguer.maxItens} ingredientes</p>
         </div>
-        <ExtraItens description={'Queijo Cheddar'} value={4.99} />
-        <ExtraItens description={'Cebola Crispy'} value={1.5} />
-        <ExtraItens description={'Molho Cheddar'} value={3.5} />
-        <ExtraItens description={'Molho de Picanha'} value={3.5} />
+
+        {hamburguer.extras.map((item: any) => {
+          return <ExtraItens key={item.id} description={item?.nm_item} value={item?.vl_item} />;
+        })}
+
         <div className='c-body__ingredients__c-extras'>
-          <p>Precisa de Talher?</p>
+          <p>{hamburguer.cutlery}</p>
         </div>
 
         <div className='c-body__ingredients__c-extras__cut'>
@@ -46,8 +80,19 @@ export function Body() {
           </div>
         </div>
         <div className='c-body__ingredients__cutlery'>
-          <PlusLessButton />
-          <AddButton />
+          <PlusMinusButton
+            onChange={(value) => {
+              return `${(qty = value)}`;
+            }}
+          />
+
+          <Button
+            onClick={() => {
+              addProduct(`${qty}`);
+            }}
+          >
+            Adicionar
+          </Button>
         </div>
       </div>
     </main>
---
+++ src/components/IsLoading/index.tsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import './styles.css';
+
+export default function LoadingSpinner() {
+  return (
+    <div className='c-spinner'>
+      <div className='c-spinner__loading'></div>
+    </div>
+  );
+}
